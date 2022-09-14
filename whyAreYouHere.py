d1e2861367bd53c8001ebb2a85576365a1d066ba
YuxinSun0 <93167785+YuxinSun0@users.noreply.github.com>
2022-09-13 19:09:38 -0700

    Create whyAreYouHere.py

---
+++ whyAreYouHere.py
@@ -0,0 +1,290 @@
+try:
+	try:
+		if __name__ == '__main__':
+			try:
+				from random import *
+				class cube:
+					orange = ['orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange', 'orange']
+					green = ['green', 'green', 'green', 'green', 'green', 'green', 'green', 'green', 'green']
+					red = ['red', 'red', 'red', 'red', 'red', 'red', 'red', 'red', 'red']
+					blue = ['blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue', 'blue']
+					white = ['white', 'white', 'white', 'white', 'white', 'white', 'white', 'white', 'white']
+					yellow = ['yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow', 'yellow']
+					
+					try:
+						def __init__(self):
+							try:
+								print('Cube Created! Use Method cubeHelp For Help.')
+							except:
+								try:
+									return 'Cube Created! Use Method cubeHelp For Help.'
+								except:
+									try:
+										print('ERROR INIT')
+									except:
+										None
+					except:
+						print('ERROR INIT')
+					
+					def r(self):
+						try:
+							a = [self.orange[2], self.orange[5], self.orange[8]]
+							b = [self.white[2], self.white[5], self.white[8]]
+							c = [self.red[2], self.red[5], self.red[8]]
+							d = [self.yellow[2], self.yellow[5], self.yellow[8]]
+							self.white[2] = a[0]
+							self.white[5] = a[1]
+							self.white[8] = a[2]
+							self.red[0] = b[0]
+							self.red[3] = b[1]
+							self.red[6] = b[2]
+							self.yellow[2] = c[0]
+							self.yellow[5] = c[1]
+							self.yellow[8] = c[2]
+							self.orange[2] = d[0]
+							self.orange[5] = d[1]
+							self.orange[8] = d[2]
+							e = [a.green[0], a.self[1], self.green[2], self.green[3], self.green[4], self.green[5], self.green[6], self.green[7], self.green[8]]
+							self.green[0] = e[6]
+							self.green[1] = e[3]
+							self.green[2] = e[0]
+							self.green[3] = e[7]
+							self.green[5] = e[1]
+							self.green[6] = e[8]
+							self.green[7] = e[5]
+							self.green[8] = e[2]
+						except:
+							try:
+								print('ERROR MOVE')
+							except:
+								None
+					
+					def ra(self):
+						a = [self.orange[2], self.orange[5], self.orange[8]]
+						b = [self.white[2], self.white[5], self.white[8]]
+						c = [self.red[2], self.red[5], self.red[8]]
+						d = [self.yellow[2], self.yellow[5], self.yellow[8]]
+						self.yellow[2] = a[0]
+						self.yellow[5] = a[1]
+						self.yellow[8] = a[2]
+						self.orange[2] = b[0]
+						self.orange[5] = b[1]
+						self.orange[8] = b[2]
+						self.white[2] = c[0]
+						self.white[5] = c[1]
+						self.white[8] = c[2]
+						self.red[0] = d[0]
+						self.red[3] = d[1]
+						self.red[6] = d[2]
+						e = [a.green[0], a.self[1], self.green[2], self.green[3], self.green[4], self.green[5], self.green[6], self.green[7], self.green[8]]
+							self.green[0] = e[]
+							self.green[1] = e[]
+							self.green[2] = e[]
+							self.green[3] = e[]
+							self.green[5] = e[]
+							self.green[6] = e[]
+							self.green[7] = e[]
+							self.green[8] = e[]
+					
+					def l(self):
+						a = [self.orange[0], self.orange[3], self.orange[6]]
+						b = [self.white[0], self.white[3], self.white[6]]
+						c = [self.red[0], self.red[3], self.red[6]]
+						d = [self.yellow[0], self.yellow[3], self.yellow[6]]
+						self.yellow[0] = a[0]
+						self.yellow[3] = a[1]
+						self.yellow[6] = a[2]
+						self.orange[0] = b[0]
+						self.orange[3] = b[1]
+						self.orange[6] = b[2]
+						self.white[0] = c[0]
+						self.white[3] = c[1]
+						self.white[6] = c[2]
+						self.red[2] = d[0]
+						self.red[5] = d[1]
+						self.red[8] = d[2]
+					
+					def la(self):
+						a = [self.orange[0], self.orange[3], self.orange[6]]
+						b = [self.white[0], self.white[3], self.white[6]]
+						c = [self.red[0], self.red[3], self.red[6]]
+						d = [self.yellow[0], self.yellow[3], self.yellow[6]]
+						self.white[0] = a[0]
+						self.white[3] = a[1]
+						self.white[6] = a[2]
+						self.red[2] = b[0]
+						self.red[5] = b[1]
+						self.red[8] = b[2]
+						self.yellow[0] = c[0]
+						self.yellow[3] = c[1]
+						self.yellow[6] = c[2]
+						self.orange[0] = d[0]
+						self.orange[3] = d[1]
+						self.orange[6] = d[2]
+					
+					def r2(self):
+						self.r()
+						self.r()
+					
+					def ra2(self):
+						self.ra()
+						self.ra()
+					
+					def l2(self):
+						self.l()
+						self.l()
+					
+					def la2(self):
+						self.la()
+						self.la()
+					
+					def u(self):
+						a = [self.orange[0], self.orange[1], self.orange[2]]
+						b = [self.green[0], self.green[1], self.green[2]]
+						c = [self.red[0], self.red[1], self.red[2]]
+						d = [self.blue[0], self.blue[1], self.blue[2]]
+						self.green[0] = c[0]
+						self.green[1] = c[1]
+						self.green[2] = c[2]
+						self.red[0] = d[0]
+						self.red[1] = d[1]
+						self.red[2] = d[2]
+						self.blue[0] = a[0]
+						self.blue[1] = a[1]
+						self.blue[2] = a[2]
+						self.orange[0] = b[0]
+						self.orange[1] = b[1]
+						self.orange[2] = b[2]
+					
+					def ua(self):
+						try:
+							a = [self.orange[0], self.orange[1], self.orange[2]]
+							b = [self.green[0], self.green[1], self.green[2]]
+							c = [self.red[0], self.red[1], self.red[2]]
+							d = [self.blue[0], self.blue[1], self.blue[2]]
+							self.green[0] = a[0]
+							self.green[1] = a[1]
+							self.green[2] = a[2]
+							self.red[0] = b[0]
+							self.red[1] = b[1]
+							self.red[2] = b[2]
+							self.blue[0] = c[0]
+							self.blue[1] = c[1]
+							self.blue[2] = c[2]
+							self.orange[0] = d[0]
+							self.orange[1] = d[1]
+							self.orange[2] = d[2]
+						except:
+							try:
+								print('ERROR MOVE')
+							except:
+								None
+					
+					def d(self):
+						try:
+							a = [self.orange[6], self.orange[7], self.orange[8]]
+							b = [self.green[6], self.green[7], self.green[8]]
+							c = [self.red[6], self.red[7], self.red[8]]
+							d = [self.blue[6], self.blue[7], self.blue[8]]
+							self.green[6] = a[0]
+							self.green[7] = a[1]
+							self.green[8] = a[2]
+							self.red[6] = b[0]
+							self.red[7] = b[1]
+							self.red[8] = b[2]
+							self.blue[6] = c[0]
+							self.blue[7] = c[1]
+							self.blue[8] = c[2]
+							self.orange[6] = d[0]
+							self.orange[7] = d[1]
+							self.orange[8] = d[2]
+						except:
+							try:
+								print('ERROR MOVE')
+							except:
+								None
+					
+					def da(self):
+						a = [self.orange[6], self.orange[7], self.orange[8]]
+						b = [self.green[6], self.green[7], self.green[8]]
+						c = [self.red[6], self.red[7], self.red[8]]
+						d = [self.blue[6], self.blue[7], self.blue[8]]
+						self.green[6] = c[0]
+						self.green[7] = c[1]
+						self.green[8] = c[2]
+						self.red[6] = d[0]
+						self.red[7] = d[1]
+						self.red[8] = d[2]
+						self.blue[6] = a[0]
+						self.blue[7] = a[1]
+						self.blue[8] = a[2]
+						self.orange[6] = b[0]
+						self.orange[7] = b[1]
+						self.orange[8] = b[2]
+					
+					def u2(self):
+						self.u()
+						self.u()
+					
+					def ua2(self):
+						self.ua()
+						self.ua()
+					
+					def d2(self):
+						self.d()
+						self.d()
+					
+					def da2(self):
+						self.da()
+						self.da()
+					
+					def cubescramble(self):
+						None
+					
+					def cubeScrambleGen(self):
+						scramble = []
+						moves = ['r', 'l', 'u', 'd', 'r`', 'l`', 'u`', 'd`', 'r2', 'l2', 'u2', 'd2', 'r`2', 'l`2', 'u`2', 'd`2', 'f', 'b', 'f2', 'b2', 'f`', 'b`', 'f`2', 'b`2']
+						for z in range(0, 25):
+							x = randint(0, 24)
+							scramble.append(moves[x - 1])
+						try:
+							print(f'{scramble[0]}, {scramble[1]}, {scramble[2]}, {scramble[3]}, {scramble[4]}\n{scramble[5]}, {scramble[6]}, {scramble[7]}, {scramble[8]}, {scramble[9]}\n{scramble[10]}, {scramble[11]}, {scramble[12]}, {scramble[13]}, {scramble[14]}\n{scramble[15]}, {scramble[16]}, {scramble[17]}, {scramble[18]}, {scramble[19]}\n{scramble[20]}, {scramble[21]}, {scramble[22]}, {scramble[23]}, {scramble[24]}')
+						except:
+							return f'{scramble[0]}, {scramble[1]}, {scramble[2]}, {scramble[3]}, {scramble[4]}\n{scramble[5]}, {scramble[6]}, {scramble[7]}, {scramble[8]}, {scramble[9]}\n{scramble[10]}, {scramble[11]}, {scramble[12]}, {scramble[13]}, {scramble[14]}\n{scramble[15]}, {scramble[16]}, {scramble[17]}, {scramble[18]}, {scramble[19]}\n{scramble[20]}, {scramble[21]}, {scramble[22]}, {scramble[23]}, {scramble[24]}'
+					
+					def cubeScrambleGenReturn(self):
+						scramble = []
+						moves = ['r', 'l', 'u', 'd', 'r`', 'l`', 'u`', 'd`', 'r2', 'l2', 'u2', 'd2', 'r`2', 'l`2', 'u`2', 'd`2', 'f', 'b', 'f2', 'b2', 'f`', 'b`', 'f`2', 'b`2']
+						for z in range(0, 25):
+							x = randint(0, 24)
+							scramble.append(moves[x - 1])
+						return f'{scramble[0]}, {scramble[1]}, {scramble[2]}, {scramble[3]}, {scramble[4]}\n{scramble[5]}, {scramble[6]}, {scramble[7]}, {scramble[8]}, {scramble[9]}\n{scramble[10]}, {scramble[11]}, {scramble[12]}, {scramble[13]}, {scramble[14]}\n{scramble[15]}, {scramble[16]}, {scramble[17]}, {scramble[18]}, {scramble[19]}\n{scramble[20]}, {scramble[21]}, {scramble[22]}, {scramble[23]}, {scramble[24]}'
+					
+					def printCube(self):
+						print(f'Orange:\n{self.orange[0]}, {self.orange[1]}, {self.orange[2]}\n{self.orange[3]}, {self.orange[4]}, {self.orange[5]}\n{self.orange[6]}, {self.orange[7]}, {self.orange[8]}\nGreen:\n{self.green[0]}, {self.green[1]}, {self.green[2]}\n{self.green[3]}, {self.green[4]}, {self.green[5]}\n{self.green[6]}, {self.green[7]}, {self.green[8]}\nRed:\n{self.red[0]}, {self.red[1]}, {self.red[2]}\n{self.red[3]}, {self.red[4]}, {self.red[5]}\n{self.red[6]}, {self.red[7]}, {self.red[8]}\nBlue:\n{self.blue[0]}, {self.blue[1]}, {self.blue[2]}\n{self.blue[3]}, {self.blue[4]}, {self.blue[5]}\n{self.blue[6]}, {self.blue[7]}, {self.blue[8]}\nWhite:\n{self.white[0]}, {self.white[1]}, {self.white[2]}\n{self.white[3]}, {self.white[4]}, {self.white[5]}\n{self.white[6]}, {self.white[7]}, {self.white[8]}\nYellow:\n{self.yellow[0]}, {self.yellow[1]}, {self.yellow[2]}\n{self.yellow[3]}, {self.yellow[4]}, {self.yellow[5]}\n{self.yellow[6]}, {self.yellow[7]}, {self.yellow[8]}')
+					
+					def returnCube(self):
+						return f'Orange:\n{self.orange[0]}, {self.orange[1]}, {self.orange[2]}\n{self.orange[3]}, {self.orange[4]}, {self.orange[5]}\n{self.orange[6]}, {self.orange[7]}, {self.orange[8]}\nGreen:\n{self.green[0]}, {self.green[1]}, {self.green[2]}\n{self.green[3]}, {self.green[4]}, {self.green[5]}\n{self.green[6]}, {self.green[7]}, {self.green[8]}\nRed:\n{self.red[0]}, {self.red[1]}, {self.red[2]}\n{self.red[3]}, {self.red[4]}, {self.red[5]}\n{self.red[6]}, {self.red[7]}, {self.red[8]}\nBlue:\n{self.blue[0]}, {self.blue[1]}, {self.blue[2]}\n{self.blue[3]}, {self.blue[4]}, {self.blue[5]}\n{self.blue[6]}, {self.blue[7]}, {self.blue[8]}\nWhite:\n{self.white[0]}, {self.white[1]}, {self.white[2]}\n{self.white[3]}, {self.white[4]}, {self.white[5]}\n{self.white[6]}, {self.white[7]}, {self.white[8]}\nYellow:\n{self.yellow[0]}, {self.yellow[1]}, {self.yellow[2]}\n{self.yellow[3]}, {self.yellow[4]}, {self.yellow[5]}\n{self.yellow[6]}, {self.yellow[7]}, {self.yellow[8]}'	
+					
+					def cubeHelp(self):
+						try:
+							print('Methods:  [cubeHelp : See The Methods,\ncubeScrambleGen : Generates a Random Scramble of 25 Moves]')
+						except:
+							print('ERROR CUBEHELP')
+				a = cube()
+				a.cubeHelp()
+				a.cubeScrambleGen()
+				a.r()
+				a.printCube()
+				a.u()
+				a.printCube()
+				a.ra()
+				a.printCube()
+				a.ua()
+				a.printCube()
+			except:
+				print('ERROR STARTING')
+	except:
+		print('ERROR START')
+except:
+	None
